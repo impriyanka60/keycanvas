1e93261b1579720706b37145cfaf10505e52474e
impriyanka60 <btech15060.22@bitmesra.ac.in>
2025-05-18 20:30:33 +0530

    3rd commit

---
+++ src/keycloak.ts
@@ -0,0 +1,10 @@
+// src/keycloak.ts
+import Keycloak from 'keycloak-js';
+
+const keycloak = new Keycloak({
+  url: 'http://localhost:8080/',
+  realm: 'whiteboard',
+  clientId: 'whiteboard-client',
+});
+
+export default keycloak;
